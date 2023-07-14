1efa8b249a196b971709dd505b8e8a8a9aa7cd38
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2023-07-14 18:50:33 +0700

    july 14

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ytdl-for-song-not-on-spotify
+It sucks when you wanna listen to a song but then you can't find it on portable music player like Spotify or Apple Music
+This likely occurs when those song are not in English
+This command line search for the song from YouTube and download an mp3 file for you, and with some certain adjustments to your Spotify settings the song will pop up automatically in your Spotify account
+## Here is how to do it:
+### Step 1:
+```git clone https://github.com/uranusisaqtpain/ytdl-for-song-not-on-spotify```
+### Step 2:
+```change your directory to the ytdl-for-song-not-on-apotify```
+### Step 3: Run the code and follow the UI instructions
+```node main.js```
---
+++ main.js
@@ -0,0 +1,53 @@
+const prompt=require("prompt-sync")({sigint:true});
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+const searchTermCLI = process.argv[2];
+
+async function getLink() {
+    const browser = await puppeteer.launch({ headless: 'new' });
+    const page = await browser.newPage();
+    await page.goto('https://www.youtube.com');
+    await page.waitForSelector('#search-input #search');
+    let searchTerm = prompt("Please enter the title of the song you want to download: ");
+    await page.type('#search-input #search', searchTerm);
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('#search-icon-legacy'),
+    ]);
+    await page.waitForSelector('ytd-video-renderer');
+    const videoTitle = await page.$eval('ytd-video-renderer h3 a#video-title', (elem) => {
+        return elem.innerText;
+    });
+    console.log({ videoTitle });
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('ytd-video-renderer h3 a#video-title'),
+    ]);
+    const videoUrl = page.url();
+    await browser.close();
+    console.log({ videoUrl });
+
+    return [videoUrl, videoTitle];
+
+}
+getLink().then(([videoUrl, videoTitle]) => {
+    async function download() {
+        if (!videoUrl || !videoUrl.startsWith('https://www.youtube.com/watch?v=')) {
+            console.log('Invalid url');
+            return;
+        }
+        const info = await ytdl.getInfo(videoUrl);
+        const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+        const bestAudioFormat = ytdl.chooseFormat(audioFormats, { quality: 'highestaudio' });
+        const fileStream = ytdl(videoUrl, { format: bestAudioFormat });
+        let filepath = prompt("Please enter the filepath in where you want to store the song: ");
+        const fileName = `${filepath}${videoTitle}.mp3`.replace(/\s/g, '_');
+        fileStream.pipe(fs.createWriteStream(fileName));
+        fileStream.on('end', function () {
+            console.log('Download finished: ' + fileName);
+        });
+    }
+    download();
+});
